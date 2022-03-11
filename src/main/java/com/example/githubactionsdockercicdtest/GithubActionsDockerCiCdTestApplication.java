d32a9259aa1bb681d1357968db42247b8eabf10d
deepredk <deepred.kim@gmail.com>
2022-03-11 09:41:45 +0900

    Initial commit

---
+++ src/main/java/com/example/githubactionsdockercicdtest/GithubActionsDockerCiCdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdockercicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDockerCiCdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsDockerCiCdTestApplication.class, args);
+    }
+
+}
